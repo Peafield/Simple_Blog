983f5551ae2205b0ba702ecdb87965a9c7d62f66
Peter J Coles <peterjohncoles@gmail.com>
2021-10-21 20:47:52 +0100

    Hide password

--- forms.py
+++ forms.py
@@ -1,10 +1,8 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, FloatField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, FloatField, PasswordField
+from wtforms.validators import DataRequired, URL
+
 
 ##WTForm
 class CreatePostForm(FlaskForm):
@@ -14,25 +12,27 @@ class CreatePostForm(FlaskForm):
     body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
 
+
 class CommentForm(FlaskForm):
     body = CKEditorField("Comment", validators=[DataRequired()])
     submit = SubmitField("Submit Comment")
 
+
 class NewUser(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
-    password = StringField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     name = StringField('Name', validators=[DataRequired()])
     submit = SubmitField('Sign Me Up')
 
+
 class LogIn(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
-    password = StringField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Let Me In!')
+
 
 class EmailForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired()])
     message = StringField("Message", validators=[DataRequired()])
     submit = SubmitField('Send')
-
-
